eee71b64ac8caf3d05fc6357514107a750aa99b3
Jeanderson1981 <163357736+Jeanderson1981@users.noreply.github.com>
2024-03-25 21:55:25 -0300

    Add files via upload

---
+++ main (2).js
@@ -0,0 +1,187 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+var numero1 = 5;
+var numero2 = 3;
+var resultado;
+
+resultado  = numero1 == numero2;
+print  ( numero1, "=", numero2, " ? ", resultado )
+
+resultado = numero1 != numero2;
+print (numero1, "!", numero2, " ? ", resultado)
+
+resultado = numero1 > numero2;
+print (numero1, ">", numero2, " ? ", resultado)
+
+ resultado = numero1 < numero2;
+print (numero1, "<", numero2, " ? ", resultado)
+
+resultado = numero1 <= numero2;
+print (numero1, "<=", numero2, " ? ", resultado)
+
+ resultado = numero1 >= numero2;
+print (numero1, ">=", numero2, " ? ", resultado)
+
+
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+var numero1 = 5;
+var numero2 = 5;
+var resultado;
+
+resultado  = numero1 == numero2;
+print  ( numero1, "=", numero2, " ? ", resultado )
+
+resultado = numero1 != numero2;
+print (numero1, "!", numero2, " ? ", resultado)
+
+resultado = numero1 > numero2;
+print (numero1, ">", numero2, " ? ", resultado)
+
+ resultado = numero1 < numero2;
+print (numero1, "<", numero2, " ? ", resultado)
+
+resultado = numero1 <= numero2;
+print (numero1, "<=", numero2, " ? ", resultado)
+
+ resultado = numero1 >= numero2;
+print (numero1, ">=", numero2, " ? ", resultado)
+
+
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+var numero1 = 5;
+var numero2 = 8;
+var resultado;
+
+resultado  = numero1 == numero2;
+print  ( numero1, "=", numero2, " ? ", resultado )
+
+resultado = numero1 != numero2;
+print (numero1, "!", numero2, " ? ", resultado)
+
+resultado = numero1 > numero2;
+print (numero1, ">", numero2, " ? ", resultado)
+
+ resultado = numero1 < numero2;
+print (numero1, "<", numero2, " ? ", resultado)
+
+resultado = numero1 <= numero2;
+print (numero1, "<=", numero2, " ? ", resultado)
+
+ resultado = numero1 >= numero2;
+print (numero1, ">=", numero2, " ? ", resultado)
+
+print ("exercicio com &&")
+
+var a1 = true && true;
+print (a1)
+var a2 = true && false;
+print (a2)
+var a3 = false && true;
+print (a3)
+var a4 = false && 3 == 4;
+print (a4)
+var a5 = "gato" && "cao";
+print (a5)
+var a6 = false && "gato";
+print (a6)
+var a7 = "gato" && false; 
+print (a7);
+
+
+print ("exercicio com ||")
+
+var a1 = true || true;
+print (a1)
+var a2 = true || false;
+print (a2)
+var a3 = false || true;
+print (a3)
+var a4 = false || 3 == 4;
+print (a4)
+var a5 = "gato" || "cao";
+print (a5)
+var a6 = false || "gato";
+print (a6)
+var a7 = "gato" || false; 
+print (a7);
+
+
+
+var idade = 43;
+var status=idade >= 18?"adulto" : "menor de idade";
+print(status);
+
+
+var palavra1 = 'Alfa';
+var palavra2 = "beto";
+var resultado = palavra1 + palavra2;
+print (resultado)
+
+/*
+var a = 15;
+var b = 5;
+
+if ((a>10 && a <20 ) || b ==5)
+print ("true");
+else
+print ("false") */
+
+/*
+var a = 15;
+var b = 5;
+
+if ((a>10) || b==5)
+print ("true");
+else
+print ("false");
+
+*/
+/*
+var a = 15;
+var b = 5;
+
+if ((a>10 && a <20 ) || b ==5 && c ==10)
+print ("true");
+else
+print ("false")
+*/
+/*
+var a = 15;
+var b = 5;
+
+if ((a>10 && b ==5) || c !==10)
+print ("true");
+else
+print ("false")
+
+*/
+
+var a = 15;
+var b = 5;
+var c = 12;
+var d = 18;
+
+if ((a>10 || b ==5 ) && c !=10 && d !=20)
+print ("true");
+else
+print ("false")
